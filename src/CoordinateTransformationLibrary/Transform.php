ba7aae296333cc516c748cfac7cc2eb8c2c909b8
Matjaž Drolc <mdrolc@gmail.com>
2016-10-10 12:03:34 +0200

    Added WGS84ToSWEREF99 method

--- src/CoordinateTransformationLibrary/Transform.php
+++ src/CoordinateTransformationLibrary/Transform.php
@@ -4,6 +4,8 @@ namespace Drola\CoordinateTransformationLibrary;
 
 use Drola\CoordinateTransformationLibrary\Position\RT90Position;
 use Drola\CoordinateTransformationLibrary\Position\SWEREF99Position;
+use Drola\CoordinateTransformationLibrary\Position\WGS84Position;
+use Drola\CoordinateTransformationLibrary\Projection\SWEREFProjection;
 
 class Transform
 {
@@ -19,5 +21,12 @@ class Transform
         $position = new SWEREF99Position($latitude, $longitude);
         $wgsPos = $position->toWGS84();
         return array($wgsPos->getLatitude(), $wgsPos->getLongitude());
+    }
+
+    public static function WGS84ToSWEREF99($latitude, $longitude)
+    {
+        $position = new WGS84Position($latitude, $longitude);
+        $rtPos = new SWEREF99Position($position, SWEREFProjection::SWEREF_99_TM);
+        return array($rtPos->getLatitude(), $rtPos->getLongitude());
     }
 }
