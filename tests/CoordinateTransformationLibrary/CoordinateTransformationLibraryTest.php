c43d3be63d94d58bcf72198fda5d93d848c7a155
Nicholas Ruunu <nicholas@ruu.nu>
2017-04-28 14:52:31 +0200

    Adds some more tests for RT90 to WGS84 convertion

--- tests/CoordinateTransformationLibrary/CoordinateTransformationLibraryTest.php
+++ tests/CoordinateTransformationLibrary/CoordinateTransformationLibraryTest.php
@@ -30,28 +30,31 @@ use Drola\CoordinateTransformationLibrary\ParseException;
 
 class CoordinateTransformationLibraryTest extends \PHPUnit_Framework_TestCase
 {
-    public function testRT90ToWGS84()
+    /**
+     * @dataProvider coordinatesRT90ToWGS84
+     */
+    public function testRT90ToWGS84($rt90Lat, $rt90Lng, $wgs84Lat, $wgs84Lng)
     {
-        $position = new RT90Position(6583052, 1627548);
+        $position = new RT90Position($rt90Lat, $rt90Lng);
         $wgsPos = $position->toWGS84();
 
-        // Values from Hitta.se for the conversion
-        $latFromHitta = 59.3489;
-        $lonFromHitta = 18.0473;
+//        // Values from Hitta.se for the conversion
+//        $latFromHitta = 59.3489;
+//        $lonFromHitta = 18.0473;
+//
+//        $lat = ((double) round($wgsPos->getLatitude() * 10000)) / 10000;
+//        $lon = ((double) round($wgsPos->getLongitude() * 10000)) / 10000;
+//
+//        $this->assertEquals($latFromHitta, $lat); //TODO: fix rounding according to extra parameter: 0.00001d
+//        $this->assertEquals($lonFromHitta, $lon);
+//
+//        // String values from Lantmateriet.se, they convert DMS only.
+//        // Reference: http://www.lantmateriet.se/templates/LMV_Enkelkoordinattransformation.aspx?id=11500
+//        $latDmsStringFromLM = "N 59º 20' 56,09287\"";
+//        $lonDmsStringFromLM = "E 18º 2' 50,34806\"";
 
-        $lat = ((double) round($wgsPos->getLatitude() * 10000)) / 10000;
-        $lon = ((double) round($wgsPos->getLongitude() * 10000)) / 10000;
-
-        $this->assertEquals($latFromHitta, $lat); //TODO: fix rounding according to extra parameter: 0.00001d
-        $this->assertEquals($lonFromHitta, $lon);
-
-        // String values from Lantmateriet.se, they convert DMS only.
-        // Reference: http://www.lantmateriet.se/templates/LMV_Enkelkoordinattransformation.aspx?id=11500
-        $latDmsStringFromLM = "N 59º 20' 56,09287\"";
-        $lonDmsStringFromLM = "E 18º 2' 50,34806\"";
-
-        $this->assertEquals($latDmsStringFromLM, $wgsPos->latitudeToString(WGS84Format::DEGREES_MINUTES_SECONDS));
-        $this->assertEquals($lonDmsStringFromLM, $wgsPos->longitudeToString(WGS84Format::DEGREES_MINUTES_SECONDS));
+        $this->assertEquals($wgs84Lat, $wgsPos->latitudeToString(WGS84Format::DEGREES_MINUTES_SECONDS));
+        $this->assertEquals($wgs84Lng, $wgsPos->longitudeToString(WGS84Format::DEGREES_MINUTES_SECONDS));
 
     }
 
@@ -142,5 +145,18 @@ class CoordinateTransformationLibraryTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(62.176, $lat_s);
         $this->assertEquals(15.903, $lon_s);
+    }
+
+    public function coordinatesRT90ToWGS84()
+    {
+        return [
+            [6580801.472, 1628658.401, 'N 59º 19\' 42,21012"', 'E 18º 3\' 55,72708"'],
+            [6168499, 1321755, 'N 55º 36\' 47,50186"', 'E 12º 58\' 34,42478"'],
+            [6581293, 1633716, 'N 59º 19\' 52,43053"', 'E 18º 9\' 16,39123"'],
+            [7538550, 1684280, 'N 67º 52\' 52,57728"', 'E 20º 11\' 30,08068"'],
+            [6393271.563, 1648713.771, 'N 57º 38\' 23,59005"', 'E 18º 17\' 43,76377"'],
+            [6296267.802, 1588822.449, 'N 56º 47\' 5,58435"', 'E 17º 15\' 30,51645"'],
+            [6404198, 1272073, 'N 57º 42\' 16,88417"', 'E 11º 58\' 51,38945"'],
+        ];
     }
 }
